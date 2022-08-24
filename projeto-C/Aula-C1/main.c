e644bda9fcdc723306d97251ff3fa444eead06d7
MayconMaiaDev <86531134+MayconMaiaDev@users.noreply.github.com>
2022-08-23 21:56:58 -0300

    update
    Aula de C (primeira)

---
+++ projeto-C/Aula-C1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int contador = 1;
+  while (contador <= 10)
+  {
+    printf("ads da depre\n" , contador);
+
+
+    contador++;
+  }
+
+  return 0;
+}
